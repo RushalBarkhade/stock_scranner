3b7d2c8b8862e0876567f3977b4504b1467037fb
rushal barkhade <rushal@e-arth.in>
2022-06-22 15:22:47 +0530

    ticker model

--- model.py
+++ model.py
@@ -4,11 +4,9 @@ from .database import Base
 
 
 class Record(Base):
-    __tablename__ = "Records"
-
-    id = Column(Integer, primary_key=True, index=True)
-    date = Column(Date)
-    country = Column(String(255), index=True)
-    cases = Column(Integer)
-    deaths = Column(Integer)
+    __tablename__ = "prices"
+    date = Column(Date, primary_key=True)
+    ticker = Column(String(255), primary_key=True)
+    price = Column(Integer)
+    volume = Column(Integer)
     recoveries = Column(Integer)
